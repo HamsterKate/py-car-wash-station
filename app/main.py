be99dbc5ca6f5ebb436d8e4800ff71a96a1770f2
Kate <hamster.k.po@gmail.com>
2026-02-10 19:21:39 +0200

    extra lines between methods removed

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self, comfort_class: int,
+                 clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
-
 
 class CarWashStation:
     def __init__(self, distance_from_city_center: float,
@@ -14,11 +14,9 @@ class CarWashStation:
         self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
 
-
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = int(self.clean_power)
-
 
     def calculate_washing_price(self, car: Car) -> float:
         if car.clean_mark >= self.clean_power:
@@ -29,7 +27,6 @@ class CarWashStation:
                  self.distance_from_city_center)
         return round(price, 1)
 
-
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0
         for car in cars:
@@ -37,7 +34,6 @@ class CarWashStation:
                 income += self.calculate_washing_price(car)
                 self.wash_single_car(car)
         return round(income, 1)
-
 
     def rate_service(self, rate: float):
         total_rating = self.average_rating * self.count_of_ratings
